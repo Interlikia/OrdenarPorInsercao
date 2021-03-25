1b12a2bb3cf9a61ac92826ad92f5f5ad932fe8bc
Interlikia <78322326+Interlikia@users.noreply.github.com>
2021-03-25 11:57:36 -0300

    Adicionar arquivos de projeto.

---
+++ OrdenarPorInsercao/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OrdenarPorInsercao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j;
+            int chave;
+            int[] vetor = new int[10];
+            Random numAle = new Random();
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                vetor[i] = numAle.Next(50);
+            }
+
+           
+            for (int i = 1; i < vetor.Length; i++)
+            {
+                chave = vetor[i];
+                j = i - 1;
+                while((j >= 0) && (vetor[j] > chave))
+                {
+                    vetor[j + 1] = vetor[j];
+                    j = j - 1;
+                }
+                vetor[j + 1] = chave;
+            }
+
+            Console.WriteLine("");
+
+            foreach (var num in vetor)
+            {
+                Console.Write($"{num},");
+            }
+
+        }
+    }
+}
